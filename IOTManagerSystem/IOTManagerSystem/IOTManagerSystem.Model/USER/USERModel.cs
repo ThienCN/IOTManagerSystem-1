d52aed7178361a814c3c7e1491aee51d1ed29bab
ThienCN <ngocthien0709@gmail.com>
2018-08-21 00:11:27 +0700

    Thiện

---
+++ IOTManagerSystem/IOTManagerSystem/IOTManagerSystem.Model/USER/USERModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOTManagerSystem.Model.USER
+{
+    public class USERModel
+    {
+        public int id { get; set; }
+        public string ma_nguoi_dung { get; set; }
+        public string ho_ten_nguoi_dung { get; set; }
+        public string gioi_tinh { get; set; }
+        public string sdt { get; set; }
+        public string cmnd { get; set; }
+        public string email { get; set; }
+        public string dia_chi { get; set; }
+        public string avartar { get; set; }
+        public DateTime ngay_sinh { get; set; }
+        public string noi_sinh { get; set; }
+
+        public int id_role { get; set; }
+        public string ma_role { get; set; }
+        public string ten_role { get; set; }
+
+        public string mat_khau { get; set; }
+        public int id_loai_xac_thuc { get; set; }
+        public int tinh_trang { get; set; }
+        public string thoi_gian_login_gmail { get; set; }
+
+        public string ten_tham_so { get; set; }
+        public string ma_code_xac_thuc { get; set; }
+
+    }
+}
---
+++ IOTManagerSystem/IOTManagerSystem/IOTManagerSystem.Repository/Core/BaseRepository.cs
@@ -0,0 +1,114 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace IOTManagerSystem.Repository.Core
+{
+    public class BaseRepository
+    {
+        private static string _connectionString = ConfigurationManager.ConnectionStrings["IOTMANAGERSYSTEM"].ConnectionString;
+
+        public BaseRepository()
+        { }
+
+        //SELECT
+        public IEnumerable<T> Query<T>(string strSQL, CommandType command, dynamic param = null)
+        {
+
+            using (IDbConnection connection = new SqlConnection(_connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    return SqlMapper.Query<T>(connection, strSQL, param: param, commandType: command);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    return default(IEnumerable<T>);
+                }
+                finally
+                {
+                    if (connection.State != ConnectionState.Closed)
+                        connection.Close();
+                }
+            }
+        }
+
+        //INTSERT,UPDATE,DELETE
+        public bool Execute<T>(string strSQL, CommandType command, DynamicParameters param = null, DynamicParameters outParam = null)
+        {
+            CombineParameters(ref param, outParam);
+
+            using (IDbConnection connection = new SqlConnection(_connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    int kq = connection.Execute(strSQL,
+                                param,
+                                commandType: command
+                            );
+                    return kq > 0;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+                finally
+                {
+                    if (connection.State != ConnectionState.Closed)
+                        connection.Close();
+                }
+            }
+        }
+
+        //FUNCTION
+        public dynamic ResultFunction(string stringFunction)
+        {
+            using (IDbConnection connection = new SqlConnection(_connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    dynamic result = connection.Query<dynamic>(stringFunction);
+                    return result[0].result;
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+                finally
+                {
+                    if (connection.State != ConnectionState.Closed)
+                        connection.Close();
+                }
+            }
+
+        }
+
+        public DynamicParameters CreateOutputParameters()
+        {
+            DynamicParameters param = new DynamicParameters();
+            param.Add("result", string.Empty, DbType.String, ParameterDirection.InputOutput, 255);
+
+            return param;
+        }
+
+        public void CombineParameters(ref DynamicParameters param, DynamicParameters outParam = null)
+        {
+            if (outParam != null)
+            {
+                if (param != null)
+                {
+                    param = new DynamicParameters(param);
+                    ((DynamicParameters)param).AddDynamicParams(outParam);
+                }
+                else param = outParam;
+            }
+        }
+    }
+}
